8dc1c7c1c232c6fbbce51595555d1ca16cf9798f
zebra fly <flyzebra@outlook.com>
2023-05-20 23:36:31 +0800

    remove bugly

--- fota/src/main/java/com/flyzebra/fota/MyApp.java
+++ fota/src/main/java/com/flyzebra/fota/MyApp.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 
 import com.flyzebra.fota.model.Flyup;
 import com.flyzebra.utils.FlyLog;
-import com.tencent.bugly.crashreport.CrashReport;
 
 public class MyApp extends Application {
     @Override
@@ -17,13 +16,6 @@ public class MyApp extends Application {
         mainintent.setClass(getApplicationContext(), MainService.class);
         mainintent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startService(mainintent);
-
-        //腾讯BUGLY
-        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
-        strategy.setAppChannel("myChannel");  //设置渠道
-        strategy.setAppVersion("1.06");      //App的版本
-        strategy.setAppPackageName("com.flyzebra.fota");  //App的包名
-        CrashReport.initCrashReport(getApplicationContext(), "22951229ad", false, strategy);
 
         Flyup.getInstance().init(getApplicationContext());
     }
